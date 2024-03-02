41a56586420c04a4422c6eca5cb9d8596715f17c
Stephen Ho <stephen.ho.232@gmail.com>
2024-03-02 22:38:06 +0700

    Add camera following

---
+++ Assets/_Scripts/Camera/CameraFollowing.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace JustGame.Scripts.Managers
+{
+    public class CameraFollowing : MonoBehaviour
+    {
+        [SerializeField] private Transform m_cameraTransform;
+        [SerializeField] private Transform m_targetTransform;
+        [SerializeField] private float m_followingSpeed;
+        private Vector3 m_targetPos;
+
+        private bool m_canFollow;
+        
+        private void Awake()
+        {
+            var camera = FindObjectOfType<UnityEngine.Camera>();
+            if (camera == null)
+            {
+                Debug.LogError("Camera not found!");
+            }
+            m_cameraTransform = camera.transform;
+            m_canFollow = true;
+        }
+
+        public void SetPermission(bool value)
+        {
+            m_canFollow = value;
+        }
+        public void SetTarget(Transform target)
+        {
+            m_targetTransform = target;
+        }
+        private void Update()
+        {
+            if (!m_canFollow) return;
+            if (m_targetTransform == null) return;
+            m_targetPos = m_targetTransform.position;
+            m_targetPos.z = -10;
+            m_cameraTransform.position = Vector3.Lerp(m_cameraTransform.position, m_targetPos, Time.deltaTime * m_followingSpeed);
+        }
+
+        public void ResetCamera()
+        {
+            m_cameraTransform.position = new Vector3(0, 0, -10);
+        }
+    }
+}
+
